9a1a9b54c9f461f1a7d9fe30413139bc082957a6
dshdcc <2508291029@qq.com>
2019-03-05 20:12:16 +0800

    movie 组件封装

---
+++ src/components/movie/channel.vue
@@ -0,0 +1,88 @@
+<template>
+   <div class="movies_channel">
+        <ul class="channel_content">
+          <li class="channel_list">
+            <div class="list_title">类型：</div>
+            <ul class="list_content">
+              <li v-for="(item,index) of list.catInfo" :key="index"><p>{{item.catName}}</p></li>
+            </ul>
+          </li>
+          <li class="channel_list">
+            <div class="list_title">区域：</div>
+            <ul class="list_content">
+              <li v-for="(item,index) of list.sourceInfo" :key="index"><p>{{item.sourceName}}</p></li>
+            </ul>
+          </li>
+          <li class="channel_list">
+            <div class="list_title">年代：</div>
+            <ul class="list_content">
+              <li v-for="(item,index) of list.yearInfo" :key="index"><p>{{item.yearName}}</p></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+</template>
+
+<script>
+export default {
+  name:'Channel',
+  props:{
+    list:Object
+  },
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.movies_channel {
+  margin: 40px auto 0;
+  width: 1078px;
+  padding: 0 20px;
+  border: 1px solid #e5e5e5;
+  .channel_content {
+    margin: 0;
+    padding: 0;
+    .channel_list {
+      padding: 10px 0!important;
+      font-size: 14px;
+      .list_title {
+
+        color: #999;
+        float: left;
+      }
+      .list_content {
+        list-style: none;
+        margin-left: 40px;
+        li{
+          cursor: pointer;
+          display: inline-block;
+          padding: 3px 9px;
+          margin-left: 12px;
+          border-radius: 14px;
+          p{
+            color: #333;
+          }
+          p:hover {
+            color: #ef4238;
+          }
+        }
+        .active {
+          background-color: #f34d41;
+          color: #fff;
+          p{
+            color: #fff;
+          }
+        }
+      }
+    }
+    .channel_list:nth-child(n+2) {
+    border-top: 1px dotted #e5e5e5;
+  }
+
+  }
+}
+</style>
--- src/pages/movie/Movie.vue
+++ src/pages/movie/Movie.vue
@@ -10,34 +10,7 @@
         </ul>
       </div>
       <!-- 电影类型 -->
-      <div class="movies_channel">
-        <ul class="channel_content">
-          <li class="channel_list">
-            <div class="list_title">类型：</div>
-            <ul class="list_content">
-              <li class="active"><p>爱情</p></li>
-              <li><p>动作</p></li>
-              <li><p>动画</p></li>
-            </ul>
-          </li>
-          <li class="channel_list">
-            <div class="list_title">类型：</div>
-            <ul class="list_content">
-              <li class="active"><p>爱情</p></li>
-              <li><p>动作</p></li>
-              <li><p>动画</p></li>
-            </ul>
-          </li>
-          <li class="channel_list">
-            <div class="list_title">类型：</div>
-            <ul class="list_content">
-              <li class="active"><p>爱情</p></li>
-              <li><p>动作</p></li>
-              <li><p>动画</p></li>
-            </ul>
-          </li>
-        </ul>
-      </div>
+     <channel :list="channelList"></channel>
     </div>
     <!-- 电影内容 -->
     <div class="movie_content">
@@ -61,7 +34,30 @@
 </template>
 
 <script>
-export default {};
+import Channel from '@/components/movie/Channel'
+export default {
+  components:{
+    Channel
+  },
+  data() {
+    return {
+      channelList:{}
+    }
+  },
+  methods: {
+    getList() {
+      this.$api.get({},'/film/getConditionList',(res)=> {
+         this.channelList ={...res.data};
+         console.log(this.channelList);
+      },(error) => {
+        console.log(error);
+      })
+    }
+  },
+  mounted() {
+    this.getList();
+  }
+};
 </script>
 
 <style lang="less">
@@ -90,57 +86,17 @@ export default {};
     }
   }
 }
-.movies_channel {
-  margin: 40px auto 0;
-  width: 1078px;
-  padding: 0 20px;
-  border: 1px solid #e5e5e5;
-  .channel_content {
-    margin: 0;
-    padding: 0;
-    .channel_list {
-      padding: 10px 0!important;
-      font-size: 14px;
-      .list_title {
-        display: inline-block;
-        color: #999;
 
-      }
-      .list_content {
-        list-style: none;
-        display: inline-block;
-        li{
-          cursor: pointer;
-          display: inline-block;
-          padding: 3px 9px;
-          margin-left: 12px;
-          border-radius: 14px;
-          p{
-            color: #333;
-          }
-          p:hover {
-            color: #ef4238;
-          }
-        }
-        .active {
-          background-color: #f34d41;
-          color: #fff;
-          p{
-            color: #fff;
-          }
-        }
-      }
-    }
-    .channel_list:nth-child(n+2) {
-    border-top: 1px dotted #e5e5e5;
-  }
-
-  }
-}
 .movie_content {
-  margin-top: 40px;
+  width: 1120px;
+  margin: 40px auto 0;
   .movie_sort {
-
+    font-size: 14px;
+    margin-left: 20px;
+    .sort_item {
+      display: inline-block;
+      margin-right: 30px;
+    }
   }
 }
 </style>
